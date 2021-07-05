1898fe25d6b1a63ca2b359f91791cf6eabeb12a3
juicyjung <travelandi01@gmail.com>
2021-07-05 23:35:00 +0900

    add test2

--- backtest.py
+++ backtest.py
@@ -1,12 +1,12 @@
 import pyupbit
 from pandas import DataFrame
 
-df = pyupbit.get_ohlcv("KRW-ETH", interval = "minute15")
+df = pyupbit.get_ohlcv("KRW-ETH", interval = "minute5")
 
 for i in range(len(df)):
     if(i>3):
         if df.iloc[i-4]['open'] < df.iloc[i-4]['close'] and df.iloc[i-3]['open'] > df.iloc[i-3]['close'] and df.iloc[i-2]['open'] > df.iloc[i-2]['close'] and df.iloc[i-1]['open'] > df.iloc[i-1]['close']:
-            if df.iloc[i-4]['volume']*1.2 < df.iloc[i-3]['volume'] and df.iloc[i-4]['volume']*1.2 < df.iloc[i-2]['volume'] and df.iloc[i-4]['volume']*1.2 < df.iloc[i-1]['volume']:
+            if df.iloc[i-4]['volume']*1.4 < df.iloc[i-3]['volume'] and df.iloc[i-4]['volume']*1.4 < df.iloc[i-2]['volume'] and df.iloc[i-4]['volume']*1.4 < df.iloc[i-1]['volume']:
                 if df.iloc[i-4]['volume']*2.5 > df.iloc[i-3]['volume'] or df.iloc[i-4]['volume']*2.5 > df.iloc[i-2]['volume'] or df.iloc[i-4]['volume']*2.5 > df.iloc[i-1]['volume']:
                     print(df.iloc[i])
-                    print(df.iloc[i]['close'] / df.iloc[i-1]['close'])
+                    print(df.iloc[i]['close'] / df.iloc[i]['open'])
