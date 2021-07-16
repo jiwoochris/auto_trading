6517fc511007bff5ab6cea177b22eaa63c3fd276
juicyjung <travelandi01@gmail.com>
2021-07-16 23:57:44 +0900

    test

--- test.py
+++ test.py
@@ -6,3 +6,4 @@ df = pyupbit.get_ohlcv("KRW-BTC", interval = "minute5", to = "202006282005")
 for i in range(len(df)):
     if(i>3):
         print(df.iloc[i-3]['open'])
+        print(df.iloc[i-3]['close'])
